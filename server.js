4e0a3a18a7f467a5413e78eaffdf26b372e8b10a
mchl <mchlginga@gmail.com>
2025-07-13 02:11:55 +0800

    add database connection logic using mongoose

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const app = require("./app.js");
 const config = require("./config/config.js");
+const connectDB = require("./lib/database.js");
 
 const port = config.port;
+connectDB();
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
