b8b789dcb1724b799e73fcb8944f4d663113391b
mchl <mchlginga@gmail.com>
2025-07-12 12:13:36 +0800

    add utility functions for directory management

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const fs = require("fs");
+
+const {PATHS} = require("./lib/constants.js");
+const {ensureDirExist} = require("./lib/utils.js");
+
 const app = express();
+
+// basic middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// request logging (morgan)
+ensureDirExist(PATHS.logFile);
+const accessLogStream = fs.createWriteStream(path.join(PATHS.logFile), {flags: 'a'});
+app.use(morgan("combined", { stream: accessLogStream }));
+
+if (process.env.ENV === "development") {
+    app.use(morgan("dev"));
+}
 
 module.exports = app;
