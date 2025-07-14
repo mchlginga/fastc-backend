c9c2ee457a6d80d9270e1b7ef9c5d5d509b78c05
mchl <mchlginga@gmail.com>
2025-07-15 07:34:18 +0800

    feat: JWT token generator

---
+++ utils/generateToken.js
@@ -0,0 +1,9 @@
+const jwt = require("jsonwebtoken");
+
+const config = require("../config/config.js");
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, config.jwtSecret, { expiresIn: config.jwtExpiresIn });
+};
+
+module.exports = generateToken;
