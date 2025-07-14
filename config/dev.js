532a747f614ac7693e9a993fd9016804de5c0282
mchl <mchlginga@gmail.com>
2025-07-15 07:32:19 +0800

    feat: JWT config vars

--- config/dev.js
+++ config/dev.js
@@ -2,5 +2,7 @@ module.exports = {
     env: "development",
     port: process.env.PORT || 3000,
     dbName: process.env.DB_NAME || "fastc_dev",
-    mongoUri: process.env.MONGO_URI
+    mongoUri: process.env.MONGO_URI,
+    jwtSecret: process.env.JWT_SECRET,
+    jwtExpiresIn: process.env.JWT_EXPIRES_IN || "1d"
 };
--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,7 @@ module.exports = {
     env: "production",
     port: process.env.PORT || 5000,
     dbName: process.env.DB_NAME || "fastc_prod",
-    mongoUri: process.env.MONGO_URI
+    mongoUri: process.env.MONGO_URI,
+    jwtSecret: process.env.JWT_SECRET,
+    jwtExpiresIn: process.env.JWT_EXPIRES_IN || "1d"
 };
