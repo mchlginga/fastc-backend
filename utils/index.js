d8615a8962434e1719a1a87fe74c3e2c480a47c4
mchl <mchlginga@gmail.com>
2025-07-16 03:29:41 +0800

    refactor: Export utils from index file

---
+++ utils/index.js
@@ -0,0 +1,11 @@
+const ensureDirExist = require("./ensureDirExist");
+const generateToken = require("./generateToken");
+const path = require("./paths");
+const statusCodes = require("./statusCodes");
+
+module.exports = {
+    ensureDirExist,
+    generateToken,
+    path,
+    statusCodes
+};
