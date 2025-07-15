c9457e8e04819c156b5d21aa2eb28ddf205e90c0
mchl <mchlginga@gmail.com>
2025-07-16 03:57:04 +0800

    refactor: standardize response codes

--- middlewares/errorHandling.js
+++ middlewares/errorHandling.js
@@ -1,8 +1,9 @@
+const { statusCodes } = require("../utils/index");
 
 const errorHandling = (error, req, res, next) => {
     console.error("Error caught by middleware:", error.stack);
 
-    res.status(error.statusCode || 500).json({ message: `Caught an error: ${error.message}` });
+    res.status(error.statusCode || statusCodes.SERVER_ERROR).json({ message: `Caught an error: ${error.message}` });
 };
 
 module.exports = errorHandling;
