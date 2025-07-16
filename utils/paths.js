70facb88785401a5a2644d2bbac6d78c05b9f064
mchl <mchlginga@gmail.com>
2025-07-17 07:15:40 +0800

    refactor: adjust profile directory path

--- utils/paths.js
+++ utils/paths.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 const PATHS = {
     logFile: path.join(__dirname, "..", "data", "access.log"),
-    profileDir: path.join(__dirname, "..", "data", "profiles")
+    profileDir: path.join(__dirname, "..", "data", "uploads", "profiles")
 };
 
 module.exports = PATHS;
