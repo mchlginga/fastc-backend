2099e5cbea51825fe572312765ef05b3c049db18
mchl <mchlginga@gmail.com>
2025-07-17 04:01:30 +0800

    feat: add path for user profile uploads

--- utils/paths.js
+++ utils/paths.js
@@ -1,7 +1,8 @@
 const path = require("path");
 
 const PATHS = {
-    logFile: path.join(__dirname, "..", "data", "access.log")
+    logFile: path.join(__dirname, "..", "data", "access.log"),
+    profileDir: path.join(__dirname, "..", "data", "profiles")
 };
 
 module.exports = PATHS;
