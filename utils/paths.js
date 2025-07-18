4789e0e75cdeae331874a1b96f699adc800f1a9b
mchl <mchlginga@gmail.com>
2025-07-18 09:28:49 +0800

    feat: add path for certificate Handlebars template

--- utils/paths.js
+++ utils/paths.js
@@ -2,7 +2,8 @@ const path = require("path");
 
 const PATHS = {
     logFile: path.join(__dirname, "..", "data", "access.log"),
-    profileDir: path.join(__dirname, "..", "data", "uploads", "profiles")
+    profileDir: path.join(__dirname, "..", "data", "uploads", "profiles"),
+    certHbsFile: path.join(__dirname, "..", "templates", "certificate.hbs")
 };
 
 module.exports = PATHS;
