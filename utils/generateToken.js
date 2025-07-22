289e1564afb9ec6123da66760c47ef7cb5b817b7
mchl <mchlginga@gmail.com>
2025-07-23 07:26:40 +0800

    fix: update config import path

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const config = require("../config");
+const config = require("../config/index");
 
 const generateToken = (id) => {
     return jwt.sign({ id }, config.jwtSecret, { expiresIn: config.jwtExpiresIn });
